786411e5b6b1bee63d33111f92dd6cba53b31042
wedson-junior <wedsonp.medrado@gmail.com>
2026-07-14 08:36:16 -0300

    adds initial API structure with FastAPI configurations and routing

---
+++ main.py
@@ -0,0 +1,10 @@
+# para rodar o código, use o comando: uvicorn main:app --reload
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from routes.auth import auth_router
+from routes.order import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
