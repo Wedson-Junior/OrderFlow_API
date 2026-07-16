3706bff60fdb7b387adad5311fc51662157c40fb
wedson-junior <wedsonp.medrado@gmail.com>
2026-07-16 09:39:13 -0300

    adds user schema and updates account creation with new fields

--- routes/auth.py
+++ routes/auth.py
@@ -1,23 +1,34 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models.users import Users
 from database import get_db
 from main import bcrypt_context
+from schemas.users import Users_schemas 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get('/')
 async def home():
-    return {'message':'Você está na home do auth'}
+    return {'message':'You are at the auth home'}
 
 @auth_router.post('/create_account')
-async def create_account(email:str, password:str, username:str, session: Session = Depends(get_db)):
-    user = session.query(Users).filter(Users.email==email).first()
+async def create_account(userschema: Users_schemas, session: Session = Depends(get_db)):
+    user = session.query(Users).filter(Users.email==userschema.email).first()
     if user:
-        return {'message':'Faild registered user'}
+        raise HTTPException(status_code=400, detail='Faild registered user')
     else:
-        encrypted_password = bcrypt_context.hash(password)
-        new_user = Users(username=username, email=email, password=encrypted_password)
+        encrypted_password = bcrypt_context.hash(userschema.password)
+        new_user = Users(
+            username=userschema.name, 
+            email=userschema.email, 
+            password=encrypted_password,
+            active=userschema.active,
+            admin=userschema.admin
+            )
         session.add(new_user)
         session.commit()
         return {'message':'Successfully registered user'}
+
+@auth_router.post('/login')
+async def login(email:str, password:str, session: Session = Depends(get_db)):
+    return
